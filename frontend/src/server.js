83bf485b9f721fa25794f68be9a3069761c1d01b
sebinpv <sebinvarghese99999@gmail.com>
2023-05-10 18:37:02 +0530

    backend url added

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "zkart-e5it-4tqze44dy-sebinpv.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000/"
+export const backend_url = "zkart-e5it-4tqze44dy-sebinpv.vercel.app/"
