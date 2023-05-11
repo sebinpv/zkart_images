3fa698bdf8f0d836c1b1b237d5c080e4f443252c
sebinpv <sebinvarghese99999@gmail.com>
2023-05-11 09:38:27 +0530

    user edit

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://zkart-nvxw.vercel.app/api/v2";
+export const server = "https://zkart.vercel.app/api/v2";
 
-export const backend_url = "https://zkart-nvxw.vercel.app/"
+export const backend_url = "https://zkart.vercel.app/"
