5be09db219d58d28c0f2ed19afc2a9c9bba3df91
sebinpv <sebinvarghese99999@gmail.com>
2023-05-11 08:42:59 +0530

    backend url added

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "zkart-nvxw-sebinpv.vercel.app",
     credentials: true,
   })
 );
