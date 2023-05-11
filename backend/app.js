31fdb13327ca6c6d7086178b4806f84e47070eaa
sebinpv <sebinvarghese99999@gmail.com>
2023-05-11 11:05:51 +0530

    all

--- backend/app.js
+++ backend/app.js
@@ -4,17 +4,18 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://zkart-nvxw.vercel.app/",
+    origin: 'https://zkart-nvxw.vercel.app',
     credentials: true,
   })
 );
-app.use("/", express.static("uploads"));
-app.use("/", (req, res)=>{
+app.use("/", express.static(path.join(__dirname,"./uploads")));
+app.use("/test", (req, res)=>{
   res.send("Hello World!!!")
 });
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
