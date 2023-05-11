03744d82d7e852fc65934fd7cdb642aa681ffcb3
sebinpv <sebinvarghese99999@gmail.com>
2023-05-11 09:49:40 +0530

    path update

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "zkart-nvxw-sebinpv.vercel.app",
+    origin: "https://zkart-nvxw.vercel.app/",
     credentials: true,
   })
 );
